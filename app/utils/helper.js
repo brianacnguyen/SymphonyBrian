bb1258400b0468259dd746df87da4cd8987424a0
brianacnguyen <brianacnguyen@gmail.com>
2017-04-08 20:56:50 -0700

    adding store

--- app/utils/helper.js
+++ app/utils/helper.js
@@ -3,5 +3,17 @@ module.exports = {
         return Object.keys(object).map(function (key) {
             return callback(key, object[key]);
         });     
-    }
+    },
+    centsToDollars: function(n) {
+        n = Number(n)/100;
+        return "$" + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+    },
+    calculateRetailPriceFromWholesale: function(wholesalePrice) {
+        var retailPrice = (wholesalePrice/75)*100;
+        return retailPrice; 
+    },
+    calculateWholesalePriceFromRetail: function(retailPrice) {
+        var wholesalePrice = (retailPrice/100)*75;
+        return wholesalePrice; 
+    },
 }
