44ed0fa9815f94061fdd5d77e243972b84073ca3
Brian August Nguyen <brianacnguyen@gmail.com>
2017-04-08 20:20:44 -0700

    initializing git

---
+++ app/components/Main.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+
+var Main = (props) => {
+    return (
+            <div> 
+                Main
+                {props.children}
+            </div>
+        );
+}
+
+module.exports = Main;
